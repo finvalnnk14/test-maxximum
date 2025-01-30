087e128a6b9b5ed8192216fce746963aa7f677e3
Fina Valentina <finavalenn1402@gmail.com>
2025-01-30 19:44:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <nav class="bg-blue-600 p-4 shadow-md fixed top-0 right-0 w-full sm:w-auto">
+    <div class="container mx-auto flex justify-between items-center">
+      <!-- Gambar di sebelah kiri -->
+      <div class="logo">
+        <img src="/src/assets/logo.jpeg" alt="Logo" class="h-8">
+      </div>
+      
+      <!-- Menu di sebelah kanan -->
+      <ul class="flex space-x-6">
+        <li v-for="(item, index) in menuItems" :key="index">
+          <a :href="item.link" class="text-black hover:text-gray-300 transition">{{ item.name }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <h1>Navigating the <br /> digital landscape <br /> for success </h1>
+  <img src="/src/assets/illustration.jpg" alt="Digital Success" className="illustration" width="500">
+  <div class="consultation-box">Book a consultation</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        { name: "About Us", link: "#about" },
+        { name: "Services", link: "#services" },
+        { name: "Use Cases", link: "#use-cases" },
+        { name: "Pricing", link: "#pricing" },
+        { name: "Blog", link: "#blog" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  z-index: 999;
+}
+
+ul {
+  display: flex;
+  list-style-type: none;
+  padding-left: 0;
+}
+
+li {
+  margin-right: 20px;
+}
+
+a {
+  text-decoration: none;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between; /* Membuat konten di kiri dan kanan */
+  align-items: center;
+  width: 100%;
+}
+
+.logo img {
+  height: 20px; /* Sesuaikan ukuran gambar */
+}
+  h1 {
+    
+            text-align: left; /* Menjadikan teks berada di sebelah kiri */
+            font-size: 60px; /* Ukuran teks 40 cm */
+             margin-left: -5cm;
+             margin-top: -4cm;
+        }
+.illustration {
+  margin-top: -5cm; /* Sesuaikan sesuai kebutuhan */
+}
+.consultation-box {
+            background-color: black;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 5px;
+            display: inline-block;
+            font-size: 18px;
+            width: 10cm;
+            font-weight: bold;
+            text-align: center;
+            cursor: pointer;
+            margin-left: -5cm;
+        }
+        
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="bg-blue-600 text-white p-4 flex items-center justify-between">
+    <button @click="toggleDrawer" class="text-white focus:outline-none">
+      &#9776;
+    </button>
+    <h1 class="text-lg font-semibold">My Website</h1>
+    <nav class="hidden md:flex space-x-4">
+      <button v-for="(item, index) in navItems" :key="index" class="hover:underline">
+        {{ item }}
+      </button>
+    </nav>
+  </header>
+  
+  <div v-if="drawerOpen" class="fixed inset-0 bg-gray-800 bg-opacity-50 z-50" @click="toggleDrawer"></div>
+  
+  <aside :class="{'translate-x-0': drawerOpen, '-translate-x-full': !drawerOpen}"
+         class="fixed top-0 left-0 w-64 h-full bg-white shadow-lg transform transition-transform duration-300 ease-in-out z-50">
+    <button @click="toggleDrawer" class="block p-4 text-right text-gray-700">&times;</button>
+    <ul class="p-4 space-y-4">
+      <li v-for="(item, index) in navItems" :key="index">
+        <button @click="toggleDrawer" class="w-full text-left p-2 hover:bg-gray-200">
+          {{ item }}
+        </button>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const drawerOpen = ref(false);
+    const navItems = ref(['Home', 'About', 'Contact']);
+
+    const toggleDrawer = () => {
+      drawerOpen.value = !drawerOpen.value;
+    };
+
+    return { drawerOpen, navItems, toggleDrawer };
+  }
+};
+</script>
+
+<style>
+/* Tambahan untuk animasi menu */
+</style>
